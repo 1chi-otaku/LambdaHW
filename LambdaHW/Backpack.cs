9463e24138e7e6fd015d3699f62765d79b968e48
Serhii <mywaifuqueen@gmail.com>
2023-02-21 22:06:01 +0000

    Add project files.

---
+++ LambdaHW/Backpack.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.CompilerServices;
+
+namespace LambdaHW
+{
+    delegate void Delegate();
+    struct BackPackObject
+    {
+
+        public string name { get; set; }
+        public double capacity { get; set; }
+        public BackPackObject(string name, double capacity)
+        {
+            this.name = name;
+            this.capacity = capacity;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Object - " + name);
+            Console.WriteLine("Capacity - " + capacity);
+        }
+    }
+    class Backpack
+    {
+        public event Delegate AddItem;
+        Color colour;
+        String creator;
+        String textile;
+        double weight;
+        double capacity;
+        BackPackObject[] objects { get; set; }
+
+        public Backpack()
+        {
+            colour = Color.Black;
+            creator = "N/A";
+            textile = "N/A";
+            weight = 1.0;
+            capacity = 0.8;
+            objects = new BackPackObject[0];
+        }
+        public Backpack(Color colour, string creator, string textile, double weight, double capacity, BackPackObject[] objects)
+        {
+            this.colour = colour;
+            this.creator = creator;
+            this.textile = textile;
+            this.weight = weight;
+            this.capacity = capacity;
+            this.objects = objects;
+        }
+        public Backpack(Color colour, string creator, string textile, double weight, double capacity, int size)
+        {
+            this.colour = colour;
+            this.creator = creator;
+            this.textile = textile;
+            this.weight = weight;
+            this.capacity = capacity;
+            objects = new BackPackObject[size];
+        }
+        public void Init()
+        {
+            Console.WriteLine("Enter creator:");
+            creator = Console.ReadLine();
+            Console.WriteLine("Enter textile:");
+            textile = Console.ReadLine();
+            Console.WriteLine("Enter weight:");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter capacity:");
+            capacity = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < objects.Length; i++)
+            {
+                Console.WriteLine("Object #:" + i + 1);
+                Console.Write("Enter name: ");
+                objects[i].name = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter capacity: ");
+                objects[i].capacity = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Creator - " + creator);
+            Console.WriteLine("Textile - " + textile);
+            Console.WriteLine("Weight - " + weight);
+            Console.WriteLine("Capacity - " + capacity);
+            Console.WriteLine("Colour - " + colour.Name);
+            Console.WriteLine("-------------------------\nBackpack content:");
+            for (int i = 0; i < objects.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ") ");
+                objects[i].Print();
+            }
+            Console.WriteLine("-------------------------");
+        }
+
+        public void AddAnItem(BackPackObject obj)
+        {
+            if (this.capacity - obj.capacity < 0)
+            {
+                throw new Exception("Capacity Exception");
+            }
+            BackPackObject[] temp = new BackPackObject[objects.Length + 1];
+            int i = 0;
+            for (i = 0; i < temp.Length - 1; i++)
+            {
+                temp[i] = objects[i];
+            }
+            temp[i] = obj;
+            objects = temp;
+            this.capacity -= obj.capacity;
+            AddItem?.Invoke();
+        }
+
+
+        //    public static Backpack operator +(Backpack op1, BackPackObject op2)
+        //    {
+        //        if (op1.capacity - op2.capacity < 0)
+        //        {
+        //            throw new Exception("Capacity Exception");
+        //        }
+        //        BackPackObject[] temp = new BackPackObject[op1.objects.Length + 1];
+        //        int i = 0;
+        //        for (i = 0; i < temp.Length-1; i++)
+        //        {
+        //            temp[i] = op1.objects[i];
+        //        }
+        //        temp[i] = op2;
+        //        op1.objects = temp;
+        //        op1.capacity -= op2.capacity;
+        //        return op1;
+        //    }
+        //}
+    }
+    //class Observer
+    //{
+    //    public static void ObjectAdd()
+    //    {
+    //        Console.WriteLine("Object was added!");
+    //    }
+    //}
+}
---
+++ LambdaHW/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaHW
+{
+    delegate string StrColDel (Color colour);
+    class Program
+    {
+        enum Colours {White = 1, Black, Red, Green, Blue,Yellow,Purple};
+        delegate void VoidVoidDel();
+        delegate void ArrVoidDel(int[] arr);
+        delegate int ArrIntDel(int[] arr);
+        delegate bool StrBoolDel(string str, string substring);
+        static void Main(string[] args)
+        {
+            #region #1  Создайте анонимный метод для получения RGB значения для цвета радуги. Цвет радуги передаётся в качестве параметра.Напишите код для тестирования работы метода
+            //StrColDel mydel = delegate (Color colour)
+            //{
+            //    string rgb = "";
+            //    rgb += colour.R.ToString();
+            //    rgb += ",";
+            //    rgb += colour.G.ToString();
+            //    rgb += ",";
+            //    rgb += colour.B.ToString();
+            //    return rgb;
+            //};
+
+            //Console.WriteLine(mydel(Color.DarkCyan));
+            #endregion
+            #region #2
+            //try
+            //{
+            //    VoidVoidDel mydel = delegate ()
+            //    {
+            //        Console.WriteLine("\nEvent: Object was added!\n");
+            //    };
+            //    Backpack backpack = new Backpack();
+            //    backpack.AddItem += new Delegate(mydel);
+            //    backpack.Print();
+            //    backpack.AddAnItem(new BackPackObject("Phone", 0.8));
+            //    backpack.Print();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            #endregion
+            #region #3 Создайте лямбда-выражение для подсчёта количества чисел в массиве, кратных семи. Напишите код для тестирования работы лямбды
+            //int[] array = { 1, 2, 3,7,14,15,21,28 };
+            //ArrVoidDel mulSeven = arr =>
+            //{
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+            //        if (arr[i] % 7 == 0)
+            //            Console.Write(arr[i] + "\t");
+            //    }
+            //};
+            //mulSeven(array);
+            #endregion
+            #region #4
+            //int[] array = { -1, 2, 5, 6, 1, -85, -81 };
+            //ArrIntDel positive = arr => arr.Count(x => x > 0);
+            //Console.WriteLine(positive(array));
+            #endregion
+            #region #5 Создайте лямбда-выражение для отображения уникальных отрицательных чисел в массиве. Напишите код для тестирования работы лямбды
+            //int[] array = { -1, 2, 5, 6, 1, -85, -81,-81,-81,-12 };
+            //ArrIntDel negativeunique = arr => arr.Distinct().ToArray().Count(x => x < 0);
+            //Console.WriteLine(negativeunique(array));
+            #endregion
+            #region #6 Создайте лямбда-выражение для проверки есть ли в  тексте заданное слово.Напишите код для тестированияработы лямбды
+            //string text = "The Universe has a beginning, but no end. The stars have a beginning, too, but they die of their own power.";
+            //StrBoolDel IsContaining = (str, substr) => str.Contains(substr);
+            //Console.WriteLine(IsContaining(text,"but"));
+            #endregion
+        }
+    }
+}
